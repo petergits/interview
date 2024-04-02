0126ec7b330a9ffddd92e9c7ca87fc1526f5520a
petergits <55213530+petergits@users.noreply.github.com>
2024-04-02 09:36:56 -0400

    Update login
    added async/await

--- typescript-front-end-refactoring/src/authentication_manager.ts
+++ typescript-front-end-refactoring/src/authentication_manager.ts
@@ -43,48 +43,43 @@ export class AuthenticationManager {
           return false;
         }
     }  
-    public login(username, password, rememberMe): Promise<any> {
-      return new Promise((resolve, reject) => {
-        fetch(this.baseUrl + "/login", {
-          method: "POST",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          referrerPolicy: "no-referrer",
-          body: JSON.stringify({ username, password }),
-        })
-          .then(async (response) => {
-            if (rememberMe) {
-              const token = await response.json();
-              localStorage.setItem("auth_token", token);  
-            }
-            
-            fetch(this.baseUrl + "/profile/" + username, {
-              method: "GET",
-              mode: "cors",
-              referrerPolicy: "no-referrer",
-            }).then((response) => {
-              let profile = response.json();
-              fetch(this.baseUrl + "/roles/" + username, {
-                method: "GET",
-                mode: "cors",
-                referrerPolicy: "no-referrer",
-              })
-                .then((response2) => {
-                  const groups = response.json();
-                  resolve({ profile, groups });
-                })
-                .catch((e) => {
-                  reject(e);
-                });
-            });
-          })
-          .catch((e) => {
-            reject(e);
-          });
+    public async login(username: string, password: string, rememberMe: boolean): Promise<any> {
+    try {
+      const response = await fetch(this.baseUrl + "/login", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify({ username, password }),
       });
+
+      if (rememberMe && response.ok) {
+        const token = await response.json();
+        localStorage.setItem("auth_token", token);
+      }
+
+      const profileResponse = await fetch(this.baseUrl + "/profile/" + username, {
+        method: "GET",
+        mode: "cors",
+        referrerPolicy: "no-referrer",
+      });
+      const profile = await profileResponse.json();
+
+      const groupsResponse = await fetch(this.baseUrl + "/roles/" + username, {
+        method: "GET",
+        mode: "cors",
+        referrerPolicy: "no-referrer",
+      });
+      const groups = await groupsResponse.json();
+
+      return { profile, groups };
+    } catch (error) {
+      console.error("Error during login:", error);
+      throw error;
     }
+  }
   
     public async getProfileForLoggedInUser(): Promise<any> {
       let token = localStorage.getItem("auth_token");
